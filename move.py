fec352f564e895e7dcc6419c9d2f87c606f88b85
Lampart071 <lampart071@gmail.com>
2020-07-14 00:33:22 +0200

    Added path confirmation, set path changed

--- move.py
+++ move.py
@@ -1,22 +1,17 @@
+from os import system, name
 import shutil
 import os
 
 
-def set_source_path():
-    print("Set source directory: ")
-    source_path = input()
-    # TODO: Add path validation
-    # TODO: Add path confirmation
-    return source_path
-
-
-def set_destination_path():
-    print("Set destination directory: ")
-    destination_path = input()
-    # TODO: Add path validation
-    # TODO: Add path confirmation
-    return destination_path
-
+def set_path(info):
+    print(info)
+    path = input()
+    flag_can_continue = False
+    path, flag_can_continue = path_confirmation(path, flag_can_continue)
+    if flag_can_continue:
+        return path
+    else:
+        menu()
 
 def set_format():
     print("Set file extenstion [without '.']: ")
@@ -26,8 +21,27 @@ def set_format():
     return file_ext
 
 
+def path_confirmation(path, flag_can_continue=False):
+    print(path, " <-  is that a correct path? [(Y)es, (N)o]")
+    confirmation_input = input()
+    if confirmation_input == "Y":
+        print("-> OK, proceed")
+        flag_can_continue = True
+    elif confirmation_input == "N":
+        print("Set new directory: ")
+        path = input()
+        print(path, " <-  is that a correct path? [(Y)es, (N)o]")
+        confirmation_input = input()
+        if confirmation_input == "Y":
+            print("-> OK, proceed")
+            flag_can_continue = True
+        elif confirmation_input == "N":
+            flag_can_continue = False
+    return path, flag_can_continue
+
+
 def list_files():
-    source_path = set_source_path()
+    source_path = set_path("Set source directory: ")
     file_ext = set_format()
     file_count = 0
     for root, dirs, files in os.walk(source_path):
@@ -36,37 +50,39 @@ def list_files():
                 path_file = os.path.join(root, file)
                 print(path_file)
                 file_count = file_count + 1
-    print("\nFILE COUNT: ", file_count, "\n\n")
+    print("\nFILE COUNT: ", file_count)
     return menu()
 
 
 def copy_files():
-    source_path = set_source_path()
-    destination_path = set_destination_path()
+    source_path = set_path("Set source directory: ")
+    destination_path = set_path("Set destination directory: ")
     file_ext = set_format()
-    # TODO: Add process confirmation
     for root, dirs, files in os.walk(source_path):
         for file in files:
             if file.endswith(file_ext):
                 path_file = os.path.join(root, file)
+                # TODO: Add errors management
                 shutil.copy2(path_file, destination_path)
+    print("-> COPY DONE!")
     return menu()
 
 
 def move_files():
-    source_path = set_source_path()
-    destination_path = set_destination_path()
+    source_path = set_path("Set source directory: ")
+    destination_path = set_path("Set destination directory: ")
     file_ext = set_format()
-    # TODO: Add process confirmation
     for root, dirs, files in os.walk(source_path):
         for file in files:
             if file.endswith(file_ext):
                 path_file = os.path.join(root, file)
+                # TODO: Add errors management
                 shutil.move(path_file, destination_path)
+    print("-> MOVE DONE!")
     return menu()
 
 
-def switch(argument):
+def select_operation_switch(argument):
     switcher = {
         "1": list_files,
         "2": copy_files,
@@ -77,8 +93,10 @@ def switch(argument):
 
 
 def menu():
+    print("\n")
     print("MENU\n1. List files\n2. Copy files\n3. Move files\nYour selection [1-3] : ")
-    print(switch(input()))
+    print(select_operation_switch(input()))
+
 
 if __name__ == "__main__":
     menu()
